479d7a0635f338e700c8dcb210841892a7cce19e
ClassicBirds <classicbirdsnft@gmail.com>
2025-07-04 00:57:15 +0200

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 // src/components/Header/Header.tsx
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import ConnectButton from "../connetbutton/ConnectButton";
-import Inventory from "../Common/Inventory"; // Add this import
+import Inventory from "../Common/Inventory";
 import { useAccount } from "wagmi";
 
 export function Header() {
@@ -18,30 +19,26 @@ export function Header() {
   }, []);
 
   return (
-    <header
-      className={`mx-auto sticky top-0 z-[100] w-full ${
-        scrolling ? "backdrop-blur-md" : ""
-      }`}
-    >
+    <header className={`mx-auto sticky top-0 z-[100] w-full ${scrolling ? "backdrop-blur-md" : ""}`}>
       <div className="!border-b !border-b-gray-800">
         <div className="!mx-auto max-w-7xl flex items-center justify-between px-6 md:px-16 h-[90px]">
           <Link href="/">
-            <img
+            <Image
               src="/IMG_20250327_010138_507.webp"
               width={60}
+              height={40}
               alt="logo"
               className="cursor-pointer rounded-2xl"
-              height={40}
             />
           </Link>
-
+          
           <div className="flex flex-row gap-5">
-            {isConnected && <Inventory />} {/* Add Inventory component here */}
+            {isConnected && <Inventory />}
             <ConnectButton />
-            <a
-              className="px-4 py-3 hidden md:block relative z-50 hover:opacity-55 text-black duration-200 font-medium text-[14px] rounded-2xl bg-[#00ffb4]"
-              href="https://linktr.ee/ClassicBirds"
-              target="_blank"
+            <a 
+              className="px-4 py-3 hidden md:block relative z-50 hover:opacity-55 text-black duration-200 font-medium text-[14px] rounded-2xl bg-[#00ffb4]" 
+              href="https://linktr.ee/ClassicBirds" 
+              target="_blank" 
               rel="noopener noreferrer"
             >
               Official links
