b323aa506b0e7447e069f40c6fac1c23ff585f2a
ClassicBirds <classicbirdsnft@gmail.com>
2025-07-04 22:35:02 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,41 +8,45 @@ import InventoryPopup from "../components/Common/InventoryPopup";
 export default function Home() {
   const [showInventory, setShowInventory] = useState(false);
   const { address } = useAccount();
+  
   return (
-    <div
-      id="mint"
-      className="flex flex-col min-h-[calc(100vh-200px)] px-3 justify-center items-center"
-    >
-         <InventoryPopup
-                    isOpen={showInventory}
-                    onClose={() => {
-                      setShowInventory(false);
-                    }}
-                  />
-      <div className="flex flex-row items-center gap-3 py-5">
-        <a
-          className="px-4 py-3  md:hidden relative z-50 hover:opacity-55 text-black duration-200 font-medium text-[14px] rounded-2xl bg-[#00ffb4] "
-          href="https://linktr.ee/ClassicBirds"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Official links
-        </a>
+    <>
+      <div
+        id="mint"
+        className="relative flex flex-col min-h-[calc(100vh-200px)] px-3 justify-center items-center"
+      >
+        <div className="flex flex-row items-center gap-3 py-5">
+          <a
+            className="px-4 py-3 md:hidden relative z-50 hover:opacity-55 text-black duration-200 font-medium text-[14px] rounded-2xl bg-[#00ffb4]"
+            href="https://linktr.ee/ClassicBirds"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Official links
+          </a>
 
-        <button
-          onClick={() => {
-            if (address) {
-              setShowInventory(true);
-            } else {
-              toast.info("Connect your wallet");
-            }
-          }}
-          className="px-4 py-3 md:hidden  relative z-50 hover:opacity-55 text-black duration-200 font-medium text-[14px] rounded-2xl bg-[#00ffb4]"
-        >
-          Inventory
-        </button>
+          <button
+            onClick={() => {
+              if (address) {
+                setShowInventory(true);
+              } else {
+                toast.info("Connect your wallet");
+              }
+            }}
+            className="px-4 py-3 md:hidden relative z-50 hover:opacity-55 text-black duration-200 font-medium text-[14px] rounded-2xl bg-[#00ffb4]"
+          >
+            Inventory
+          </button>
+        </div>
+        <MintContentBox />
       </div>
-      <MintContentBox />
-    </div>
+
+      <InventoryPopup
+        isOpen={showInventory}
+        onClose={() => {
+          setShowInventory(false);
+        }}
+      />
+    </>
   );
 }
