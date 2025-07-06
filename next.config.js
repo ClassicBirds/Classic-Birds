6b61704d94913888129ffd72e5df754cc76eb67f
ClassicBirds <classicbirdsnft@gmail.com>
2025-07-06 05:29:08 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,31 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-	reactStrictMode: true,
-	domains: ["https://oaidalleapiprodscus.blob.core.windows.net"],
-	webpack: (config) => {
-	  config.externals.push(
-		"pino-pretty",
-		"lokijs",
-		"encoding"
-	  );
-	return config;
-  }}
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "oaidalleapiprodscus.blob.core.windows.net",
+      "gateway.pinata.cloud",
+      "ipfs.io",
+      "cloudflare-ipfs.com",
+      "dweb.link",
+      "nftstorage.link"
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    unoptimized: true, // Disable Next.js image optimization for IPFS
+  },
+  webpack: (config) => {
+    config.externals.push(
+      "pino-pretty",
+      "lokijs",
+      "encoding"
+    );
+    return config;
+  }
+};
+
+module.exports = nextConfig;
