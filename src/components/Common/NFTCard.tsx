86cb02909f787bacbdf730c17cbde545df9b6fbd
ClassicBirds <classicbirdsnft@gmail.com>
2025-07-06 03:56:19 +0200

    Update NFTCard.tsx

--- src/components/Common/NFTCard.tsx
+++ src/components/Common/NFTCard.tsx
@@ -1,6 +1,6 @@
 // NFTCard.tsx
 import React from 'react';
-import LazyloadImage from './LazyloadImage';
+import LazyloadImage from '../Lazyload/LazyloadImage';
 
 export default function NFTCard({ id, name, image_url }: { id: string, name: string, image_url: string }) {
   // Clean up the image URL
