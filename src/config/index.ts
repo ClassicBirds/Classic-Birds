be5fadf1117c6e88fbb543df9f44144976a68d29
ClassicBirds <classicbirdsnft@gmail.com>
2025-08-23 22:49:09 +0200

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,5 @@ import { tokendata, TeamData } from "../../typeing";
 
 export const NFT_ADDR = "0x2D4e4BE7819F164c11eE9405d4D195e43C7a94c6" 
 export const NFTLIMIT = 1;
+export const NFT_HELPER_ADDR = '0xc39682b6df58aA3834836e55e0eE001A70a2f5d5'; // Your deployed helper contract address
+export const ETC_RPC_URL = 'https://etc.rivet.link'; // ETC RPC endpoint
